92c1b08c44e317a795462c6833339a13afa16161
Mehmet Ozan BALI <grely.bali.63@gmail.com>
2025-03-27 19:38:52 +0300

    Updated for nonlinear equal systems and Answers

--- Gradiant_and_Hessian_Matrixes.py
+++ Gradiant_and_Hessian_Matrixes.py
@@ -4,15 +4,15 @@
 
 import numpy as np
 def function(x):
-    f = (x[0] - 1)**2 + (x[1] - 1)**2 - x[0]*x[1] #Function
+    f = ( 1 * x[0]**3) / 3 + (1 * x[0]**2)  / 2 + 2 * x[0] * x[1] + (1* x[1]**2) / 2 - x[1] + 9 #Function
     return f
 
 def gradf(x):
-    gradf = [(2*(x[0] - 1)-x[1]), (2*(x[1] - 1)-x[0])] #Function's Gradiant Matrix Form
+    gradf = [(x[0]**2 + x[0] + 2*x[1]), (2 * x[0] + x[1] - 1 )] #Function's Gradiant Matrix Form
     return gradf
 
 def hessianf(x):
-    hessianf = np.array([[2,-1],[-1,2]]) #Funciton's Hessien matrix Form
+    hessianf = np.array([[5,2],[2,1]]) #Funciton's Hessien matrix Form
     return hessianf
 
 x = [1,1]
@@ -22,9 +22,13 @@ loop = True
 while loop:
     i += 1
     x = x - 0.1 *  np.array(gradf(x))
-    normGradf = np.linalg.norm(gradf(x))
-    print("i:",i," f(x):",function(x), "//Gradf//:",normGradf)
 
+    normGradf = np.linalg.norm(gradf(x))
+
+    if normGradf > 1e10:
+        print("Gradient norm is too large, stopping the loop.")
+        loop = False
+    print("i:",i," f(x):",function(x), "//Gradf//:",normGradf)
     if normGradf < 1e-6:
         loop = False
 
